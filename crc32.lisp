600bb0c06db7e5c33f6d5c6cd487cbb2c26424e5
Sebastian Melzer <semz@semelz.de>
2023-04-02 09:59:54 +0200

    Make basic/sliced CRC-32 machinery reusable

--- crc32.lisp
+++ crc32.lisp
@@ -25,62 +25,68 @@
           (setf (aref result i) next))))
   :test 'equalp)
 
-(defun basic-update-crc-32 (data start end state)
-  (declare (type buffer data)
-           (type array-length start end)
-           (type (unsigned-byte 32) state)
-           (optimize speed))
-  (assert (<= 0 start end (length data)))
-  ;; Safety 0 makes a crazy difference under ECL (2.8x) and improves CCL a bit
-  ;; as well (1.16x).
-  (locally (declare (optimize (safety 0)))
-    (loop :while (< start end) :do
-      (setf state (logxor (aref +crc-32-table+ (logand #xFF (logxor state (aref data start))))
-                          (ash state -8)))
-      (incf start)))
-  state)
+(defmacro define-basic-update-crc (function-name base-table-constant)
+  `(defun ,function-name (data start end state)
+     (declare (type buffer data)
+              (type array-length start end)
+              (type (unsigned-byte 32) state)
+              (optimize speed))
+     (assert (<= 0 start end (length data)))
+     ;; Safety 0 makes a crazy difference under ECL (2.8x) and improves CCL a
+     ;; bit as well (1.16x).
+     (locally (declare (optimize (safety 0)))
+       (loop :while (< start end) :do
+         (setf state (logxor (aref ,base-table-constant
+                                   (logand #xFF (logxor state (aref data start))))
+                             (ash state -8)))
+         (incf start)))
+     state))
 
 ;; The parameter 2 is derived empirically. 3 gives about the same performance
 ;; but requires larger tables. 1 and 4 are slower. If you wanted to do
 ;; something really cursed, you could benchmark this at compile time and
 ;; choose the implementation and parameters based on the results.
-(onetime-macrolet ((var-count 2)
-                   (vars (map-into (make-list var-count) #'gensym))
-                   (table-count (* 4 var-count))
-                   (table (make-array (list table-count 256) :element-type '(unsigned-byte 32))))
-  (assert (>= table-count 4))
-  (dotimes (i 256)
-    (setf (aref table 0 i) (aref +crc-32-table+ i)))
-  (dotimes (i 256)
-    (loop :for j :from 1 :below table-count :do
-      (setf (aref table j i)
-            (logxor (ash (aref table (- j 1) i) -8)
-                    (aref table 0 (logand #xFF (aref table (- j 1) i)))))))
+(defmacro define-fast-update-crc (function-name remainder-function base-table-constant)
+  (let* ((var-count 2)
+         (vars (map-into (make-list var-count) #'gensym))
+         (table-count (* 4 var-count))
+         (table (make-array (list table-count 256) :element-type '(unsigned-byte 32))))
+    (assert (>= table-count 4))
+    (dotimes (i 256)
+      (setf (aref table 0 i) (aref (eval base-table-constant) i)))
+    (dotimes (i 256)
+      (loop :for j :from 1 :below table-count :do
+        (setf (aref table j i)
+              (logxor (ash (aref table (- j 1) i) -8)
+                      (aref table 0 (logand #xFF (aref table (- j 1) i)))))))
 
-  `(defun fast-update-crc-32 (data start end state)
-     (declare (type buffer data)
-              (type array-length start end)
-              (type (unsigned-byte 32) state)
-              (optimize speed))
-     (let ((table ,table))
-       (declare (type (simple-array (unsigned-byte 32) (,table-count 256))))
-       (loop :while (>= (- end start) ,(* 4 var-count)) :do
-         (let (,@(loop :for v :in vars
-                       :for off :from 0 :by 4
-                       :collect `(,v (locally (declare (optimize (safety 0)))
-                                       (ub32ref/le data (+ start ,off))))))
-           (declare (type (unsigned-byte 32) ,@vars))
-           (setf ,(first vars) (logxor state ,(first vars)))
-           (setf state (logxor
-                        ,@(loop :for v :in vars
-                                :for index :downfrom table-count :by 4
-                                :collect `(aref table ,(- index 1) (ldb (byte 8 0) ,v))
-                                :collect `(aref table ,(- index 2) (ldb (byte 8 8) ,v))
-                                :collect `(aref table ,(- index 3) (ldb (byte 8 16) ,v))
-                                :collect `(aref table ,(- index 4) (ldb (byte 8 24) ,v)))))
-           (incf start ,table-count))))
-     ;; Deal with remaining bytes
-     (basic-update-crc-32 data start end state)))
+    `(defun ,function-name (data start end state)
+       (declare (type buffer data)
+                (type array-length start end)
+                (type (unsigned-byte 32) state)
+                (optimize speed))
+       (let ((table ,table))
+         (declare (type (simple-array (unsigned-byte 32) (,table-count 256))))
+         (loop :while (>= (- end start) ,(* 4 var-count)) :do
+           (let (,@(loop :for v :in vars
+                         :for off :from 0 :by 4
+                         :collect `(,v (locally (declare (optimize (safety 0)))
+                                         (ub32ref/le data (+ start ,off))))))
+             (declare (type (unsigned-byte 32) ,@vars))
+             (setf ,(first vars) (logxor state ,(first vars)))
+             (setf state (logxor
+                          ,@(loop :for v :in vars
+                                  :for index :downfrom table-count :by 4
+                                  :collect `(aref table ,(- index 1) (ldb (byte 8 0) ,v))
+                                  :collect `(aref table ,(- index 2) (ldb (byte 8 8) ,v))
+                                  :collect `(aref table ,(- index 3) (ldb (byte 8 16) ,v))
+                                  :collect `(aref table ,(- index 4) (ldb (byte 8 24) ,v)))))
+             (incf start ,table-count))))
+       ;; Deal with remaining bytes
+       (,remainder-function data start end state))))
+
+(define-basic-update-crc basic-update-crc-32 +crc-32-table+)
+(define-fast-update-crc fast-update-crc-32 basic-update-crc-32 +crc-32-table+)
 
 (defun start-crc-32 ()
   #xFFFFFFFF)
